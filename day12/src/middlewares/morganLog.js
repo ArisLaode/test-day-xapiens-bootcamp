3ce547c1997091d706068c72647e2cb4599e9600
La.Saputra-EXT <La.Saputra-EXT@xapiens.id>
2021-02-03 11:02:39 +0700

    change format datetime in morgan

--- day12/src/middlewares/morganLog.js
+++ day12/src/middlewares/morganLog.js
@@ -1,8 +1,17 @@
 "use strict";
 
 const morgan = require("morgan");
+const moment = require("moment-timezone");
 const path = require("path");
 const fs = require("fs");
+
+morgan.token("date", (req, res, tz) => {
+  return moment().tz(tz).format();
+});
+morgan.format(
+  "common",
+  '[:date[Asia/Jakarta]] ":method :url" :status :res[content-length] - :response-time ms'
+);
 
 const appLogStream = fs.createWriteStream(path.join("tmp/logDaily.log"), {
   flags: "a",
