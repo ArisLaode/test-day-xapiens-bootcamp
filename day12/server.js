273f0802e4cf6a4ddb221788e514e2ebe321f61d
La.Saputra-EXT <La.Saputra-EXT@xapiens.id>
2021-02-03 10:33:32 +0700

    change default

--- day12/server.js
+++ day12/server.js
@@ -1,9 +1,8 @@
 "use strict";
 
 const express = require("express");
-const log_morgan = require("./src/middlewares/morganlog");
 const errorHandling500 = require("./src/middlewares/errorHandling500");
-const errorHandling404 = require("./src/middlewares/errorHandling404");
+const errorHandling400 = require("./src/middlewares/errorHandling400");
 const app = express();
 
 const PORT = 3000;
@@ -11,10 +10,10 @@ const routers = require("./src/routers");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(log_morgan);
 app.use("/api/v1", routers);
+
 app.use(errorHandling500);
-app.use(errorHandling404);
+app.use(errorHandling400);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
