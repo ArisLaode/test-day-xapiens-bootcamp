3f88d352e7e1c767869c876e854b728b2e095729
La.Saputra-EXT <La.Saputra-EXT@xapiens.id>
2021-02-10 15:11:32 +0700

    xapiens deploy heroku

--- day15/src/controller/AuthorController.js
+++ day15/src/controller/AuthorController.js
@@ -6,6 +6,8 @@ const QueueMQ = require("bullmq");
 const { setQueues, BullMQadapter, BullAdapter } = require("bull-board");
 const { author } = require("../db/models");
 const baseResponse = require("../helpers/response");
+const { response } = require("express");
+var cloudinary = require("cloudinary").v2;
 
 class AuthorController {
   static async getAllDatas(req, res, next) {
@@ -94,17 +96,71 @@ class AuthorController {
       let path = "public/upload/authors/";
       let fileName = req.file.filename;
       let resultPathFileName = path + fileName;
-      const datas = await author.update(
-        {
-          photo: resultPathFileName,
-        },
-        { where: { id: req.params.id } }
+      // const datas = await author.update(
+      //   {
+      //     photo: resultPathFileName,
+      //   },
+      //   { where: { id: req.params.id } }
+      // );
+      cloudinary.config({
+        cloud_name: process.env.CLOUD_NAME,
+        api_key: process.env.API_KEY,
+        api_secret: process.env.API_SECRET,
+      });
+
+      const img = await cloudinary.uploader.upload(
+        fileName,
+        function (error, result) {
+          console.log(result, error);
+        }
       );
+
+      const data = await author.update({
+        photo: fileName,
+      });
+      // data.save().then((data) => {
+      //   response.message = "Data berhasil disimpan";
+      //   response.data = img;
+      //   res.status(201).json(response);
+      // });
     } catch (error) {
       res.status(400);
       next(err);
     }
-    return baseResponse({ message: "photo upload succes" })(res, 200);
+    return baseResponse({ message: "photo upload succes" })(data, 200);
+  }
+
+  static async uploadImage(req, res) {
+    // var filename = __dirname +'/'+ req.file;
+    let filename = req.file.filename;
+    // console.log(filename)
+    cloudinary.config({
+      cloud_name: process.env.CLOUD_NAME,
+      api_key: process.env.API_KEY,
+      api_secret: process.env.API_SECRET,
+    });
+    const img = await cloudinary.uploader.upload(
+      filename,
+      function (error, result) {
+        console.log(result, error);
+      }
+    );
+
+    const { body } = req;
+
+    const data = await author.build(body);
+    data
+      .save()
+      .then((data) => {
+        response.message = "Data berhasil disimpan";
+        response.data = img;
+        res.status(201).json(response);
+      })
+      .catch((err) => {
+        response.status = false;
+        response.message = err.message;
+        res.status(400).json(response);
+      });
   }
 }
 
